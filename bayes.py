5d3f419b6de2fde4c6f819db56f941fb15f07791
Carlos Cueto <a01209474@itesm.mx>
2018-10-08 15:59:44 -0500

    start parser

---
+++ bayes.py
@@ -0,0 +1,74 @@
+import fileinput
+from pprint import pprint
+
+class BayesNode:
+  def __init__(self, name, parents, CPT):
+    self.name = name
+    self.parents = parents
+    self.CPT = CPT
+
+
+
+def probabilitiesOf(query, nodes):
+  print('ProbabilitesOf')
+  if len(query) == 1:
+    print('One prob')
+  else:
+    print('Evidence')
+    filtered = list(
+        filter(
+            lambda x: (x[1:x.find('|')]).count(query) > 0)
+    )
+    print(filtered)
+  pass
+
+def main():
+  lines = []
+
+  for line in fileinput.input():
+    lines.append(line.rstrip())
+
+  nodes = lines[0].replace(' ', '').split(',')
+  n = int(lines[1])
+  probabilities = [item for item in lines[2:n + 2]]
+  #print(prob)
+
+  for node in nodes:
+    # Get Parents from node
+    parents = []
+    curr_node_prob = list(filter(lambda x: (x[0:x.find('|')]).count(node) > 0, probabilities))
+    count_prob = len(curr_node_prob)
+
+    if count_prob > 1:
+      for i in curr_node_prob:
+        for n in nodes:
+          if n in i and n != node and n not in parents:
+            parents.append(n)
+
+    print()
+    print('Node', node, 'Filtered', curr_node_prob)
+    print('Parents', parents)
+
+  """
+  for i in range(2, n + 2):
+    query = lines[i].split('|')
+    if len(query) == 1:
+      probabilitiesOf(query, nodes)
+    else:
+      evidence = query[1]
+      probabilitiesOf(query, nodes)
+  """
+
+  #tests = int(lines[5])
+  #pprint(lines)
+
+  #print()
+  #print('Nodes', nodes)
+  #print('Num of Probabilities', n)
+  #print('Tests', tests)
+  pass
+
+
+
+if __name__ == '__main__':
+  main()
