1e44127e1885437348191cc5fcb3037bc22d8e36
alexgamaes <alexgama02@hotmail.com>
2018-10-16 04:47:02 +0000

    Partially solved

--- bayes.py
+++ bayes.py
@@ -37,9 +37,18 @@ def generateBayesNetwork(nodes, probabilities):
     #print('Node', node)
     #print()
     for line in curr_node_prob:
-      given, prob = line.split('=')
-      given = given.split(",")
-      aux = ""
+      splitVariables, prob = line.split('=')
+      splitGiven = splitVariables.split("|")
+      
+      #print(splitGiven)
+      variable = splitGiven[0]
+      given = []
+      if len(splitGiven) > 1:
+        given = splitGiven[1].split(",")
+        given.sort(key=lambda x: x[1:])
+        
+      print(splitGiven)
+      aux = variable + "|"
       for item in given:
         #print('not', item, '!=', node)
         if item[1:] != node:
@@ -49,13 +58,13 @@ def generateBayesNetwork(nodes, probabilities):
         #  print('Key', aux)
         aux += ','
         #print()
-      #print(aux)
+      #print(curr_node_prob, len(curr_node_prob))
       if len(curr_node_prob) > 1:
         tk = '+' + aux[1:-1]
         fk = '-' + aux[1:-1]
       else:
-        tk = '+' + aux[:-1]
-        fk = '-' + aux[:-1]
+        tk = '+' + aux[1:-1]
+        fk = '-' + aux[1:-1]
       CPT[tk] = float(prob)
       CPT[fk] = 1 - float(prob)
       #print('Given', given)
@@ -76,6 +85,124 @@ def generateBayesNetwork(nodes, probabilities):
   
   return bayesian_network
 
+# Auxiliar functions
+def inEvidence(variable, evidence):
+  return ('+' + variable) in evidence or ('-' + variable) in evidence
+
+def equalToEvidence(variables, evidence):
+  for v in variables:
+    if not inEvidence(v, evidence):
+      return False
+      
+  return True
+
+
+def getTopologicalSortedVariables(variables, bayesian_network):
+  result = [v[1:] for v in variables]
+  
+  for node in result:
+    for parent in bayesian_network[node].parents:
+      if not (parent in result):
+        result.append(parent) 
+  
+  return result
+
+
+def getVariablesFromCombination(variables, n):
+  ans = []
+  
+  index = len(variables) - 1
+  while index >= 0:
+    if n % 2 == 1:
+      ans.append('+' + variables[index])
+    else:
+      ans.append('-' + variables[index])
+      
+    n //= 2
+    index -= 1
+  
+  return ans
+
+# Probability functions
+def chainRule(variables, bayesian_network):
+  ans = 1.0
+  
+  for v in variables:
+    node = bayesian_network[v[1:]]
+    
+    ctpKey = ""
+    if len(node.parents) == 0:
+      ctpKey = v
+    else:
+      given = []
+      for parent in node.parents:
+        if ('+' + parent) in variables:
+          given.append('+' + parent)
+        else:
+          given.append('-' + parent)
+          
+      given.sort(key=lambda x: x[1:])
+      given = ",".join(given)
+      
+      ctpKey = v + "|" + given
+      
+    print("KEY", ctpKey)
+    ans *= node.CPT[ctpKey]
+    
+  print(ans)
+  return ans
+      
+
+def conditionalProbability(variables, evidence, bn):
+  return totalProbability(variables + evidence, bn) / totalProbability(evidence, bn)
+
+def totalProbability(evidence, bayesian_network):
+  allNodes = getTopologicalSortedVariables(evidence, bayesian_network)
+  
+  print("ALL NODES", allNodes, evidence)
+  
+  if equalToEvidence(allNodes, evidence):
+    return chainRule(evidence, bayesian_network)
+  else:
+    notInEvidence = []
+    for node in allNodes:
+      if not inEvidence(node, evidence):
+        notInEvidence.append(node)
+        
+    limit = pow(2, len(notInEvidence))
+    
+    ans = 0
+    for combination in range(limit):
+      genVariables = getVariablesFromCombination(notInEvidence, combination)
+      
+      ans += chainRule(evidence + genVariables, bayesian_network)
+    
+        
+    print("notInEvidence, ", notInEvidence)
+    
+    return ans
+  
+  
+
+## Compute
+def enumarationAsk(x, evidence, bayesian_network):
+  
+  
+  evidence_xi = evidence[:]
+  evidence_xi.append(x)
+  
+  xi = enumerateAll(bayesian_network.keys(), bayesian_network, evidence_xi)
+  return xi
+  
+def enumerateAll(variables, bayesian_network, evidence):
+  if len(variables) == 0:
+    return 1
+  y, eyi = variables[0], variables[1:]
+  
+  if inEvidence(y, evidence):
+    pass
+  else:
+    pass
 
 def main():
   lines = []
@@ -92,6 +219,8 @@ def main():
   t = int(lines[n + 2])
   tests = [item for item in lines[(s-t):]]
   
+  print(probabilities)
+  
   bayesian_network = generateBayesNetwork(nodes, probabilities)
   
   #print('DICTIONARY JUST IN CASE')
@@ -99,7 +228,13 @@ def main():
   for key,value in bayesian_network.items():
     print('Key', key, '\n', value)
 
-  pass
+
+  for t in tests:
+    query = t.split("|")
+    if len(query) == 1:
+      print("UNOO")
+    else:
+      print(conditionalProbability(query[0].split(","), query[1].split(","), bayesian_network))
 
 
 
